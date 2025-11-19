174667ccc11a39d03419e079eee71bbe9ba4a3c2
GigaGonzalo <darkorbitb1@live.com.mx>
2025-11-19 12:39:56 -0600

    refactor: Optimiza funciones reemplazando variable global y añade comentario profesional

--- Aprendizaje/Ejercicios/ejer_calculadora.py
+++ Aprendizaje/Ejercicios/ejer_calculadora.py
@@ -5,10 +5,15 @@
 #**********************************************************************************************#
 
 historial = []
-total_ = 0.0
 
-def SumaF(pos_his):
-    global total_
+def SumaF(pos_his : int, total_ : float):
+    """
+    Suma el valor recibido al valor global de la operacion
+    
+    Args:
+        pos_his : int       Posicion de la operacion en el historial
+        total_  : float     Valor total de la operacion 
+    """
     while True:
         try:
             x = peticion_de_valor(total_, "a SUMAR ")
@@ -19,13 +24,19 @@ def SumaF(pos_his):
                 total_ += x
                 guardar_historial(pos_his, " + ", x, total_)
                 operacion = peticion_de_operacion(total_)
-                menu_operaciones(operacion, pos_his)
+                menu_operaciones(operacion, pos_his, total_)
 
         except ValueError:
             print("INGRESE UN NUMERO PARA LA OPERACION")
 
-def RestaF(pos_his):
-    global total_
+def RestaF(pos_his : int, total_ : float):
+    """
+    Resta el valor recibido al valor global de la operacion
+    
+    Args:
+        pos_his : int       Posicion de la operacion en el historial
+        total_  : float     Valor total de la operacion 
+    """
     while True:
         try:
             x = peticion_de_valor(total_, "a RESTAR ")
@@ -36,13 +47,19 @@ def RestaF(pos_his):
                 total_ -= x
                 guardar_historial(pos_his," - ", x, total_)
                 operacion = peticion_de_operacion(total_)
-                menu_operaciones(operacion, pos_his)
+                menu_operaciones(operacion, pos_his, total_)
 
         except ValueError:
             print("INGRESE UN NUMERO PARA LA OPERACION")
 
-def MultiF(pos_his):
-    global total_
+def MultiF(pos_his : int, total_ : float):
+    """
+    Multiplica el valor recibido al valor global de la operacion
+    
+    Args:
+        pos_his : int       Posicion de la operacion en el historial
+        total_  : float     Valor total de la operacion 
+    """
     while True:
         try:
             x = peticion_de_valor(total_, "a MULTIPLICAR ")
@@ -53,14 +70,20 @@ def MultiF(pos_his):
                 total_ *= x
                 guardar_historial(pos_his," * ", x, total_)
                 operacion = peticion_de_operacion(total_)
-                menu_operaciones(operacion, pos_his)
+                menu_operaciones(operacion, pos_his, total_)
                 
 
         except ValueError:
             print("INGRESE UN NUMERO PARA LA OPERACION")
 
-def DiviF(pos_his):
-    global total_
+def DiviF(pos_his : int, total_ : float):
+    """
+    Divide el valor recibido al valor global de la operacion
+    
+    Args:
+        pos_his : int       Posicion de la operacion en el historial
+        total_  : float     Valor total de la operacion 
+    """
     while True:
         try:
             x = peticion_de_valor(total_, "a DIVIDIR ")
@@ -71,7 +94,7 @@ def DiviF(pos_his):
                 total_ /= x
                 guardar_historial(pos_his," / ", x, total_)
                 operacion = peticion_de_operacion(total_)
-                menu_operaciones(operacion, pos_his)
+                menu_operaciones(operacion, pos_his, total_)
 
         except ValueError:
             print("INGRESE UN NUMERO PARA LA OPERACION")
@@ -79,49 +102,85 @@ def DiviF(pos_his):
             print("El divisor no puede ser igual a 0")
 
 def peticion_de_valor(total_ : float, operacion : str) -> str:
-    print("peti valor")
+    """
+    Solicita el valor para la operacion al usuario
+    
+    Args:
+        total_ : float        Total de la operacion en curso
+        operacion : str       Operador a realizar 
+        
+    Returns:
+        Valor ingresado, en caso de alfabeto, en minusculas
+    """
     print(f"Ingrese el valor numerico {operacion}:         Total = {total_}    x para salir ")
-    x = input()
+    x = input().lower()
     return x
 
 def peticion_de_operacion(total_ : float) -> str:
+    """
+    Solicita al usuario el siguiente operador
+    
+    Args:
+        total_ : float      Total de la operacion en curso
+        
+    Returns:
+        Operador valido
+    """
     opciones = ["+", "-", "*", "/", "x"]
     while True:
         print(f"Total = {total_}")
-        opcion = input("Ingrese signo de operacion   \n+ Suma   \n- Resta   \n* Multiplicacion  \n/ Division   \nx Salir\n")
+        opcion = input("Ingrese signo de operacion   \n+ Suma   \n- Resta   "
+                       +"\n* Multiplicacion  \n/ Division   \nx Salir\n").lower()
         if opcion in opciones:
             return opcion
         else:
             print("Ingrese un signo de operacion valido! ")
 
-def menu_operaciones(operacion : str, pos_his : int):
-    global total_
-
+def menu_operaciones(operacion : str, pos_his : int, total_ : float):
+    """
+    Menu encargado de realizar la siguiente operacion basado en el operador ingresado
+    
+    Args:
+        operacion : str         Operador de la siguiente operacion
+        pos_his   : int         Posicion de la operacion en el historial
+        total_    : float       Total de la operacion en curso
+    """
     if operacion == "+":
-        SumaF(pos_his)
+        SumaF(pos_his, total_)
     elif operacion == "-":
-        RestaF(pos_his)
+        RestaF(pos_his, total_)
     elif operacion == "*":
-        MultiF(pos_his)
+        MultiF(pos_his, total_)
     elif operacion == "/":
-        DiviF(pos_his)
+        DiviF(pos_his, total_)
     elif operacion == "x":
-        total_ = 0
-        print(historial)
         menu_principal()
 
 def guardar_historial(posicion_historial : int, operador : str, valor : float , resultado : float):
+    """
+    Almacena la operacion en el historial de la sesion
+    
+    Args:
+        posicion_historial : int    Posicion de la operacion en el historial
+        operador           : str    Operador de la operacion
+        valor              : float  Valor que se aplico en la operacion
+        resultado          : float  Total de la operacion realizada
+    """
     global historial
     operacion =  operador + str( float(valor)) + " = " + str(resultado) + " "
-    historial[posicion_historial-1] += operacion
+    historial[posicion_historial] += operacion
 
 def nueva_operacion():
-    global total_
+    """
+    Inicia una nueva operacion de la calculadora
+
+    """
     global historial
+    total_ = 0.0
     if len(historial) == 0:
         pos_his = len(historial)
         
-    else:
+    elif len(historial) > 0:
         pos_his = len(historial)
     while True:
         x = peticion_de_valor(total_, "a OPERAR ")
@@ -131,17 +190,21 @@ def nueva_operacion():
             total_ += float(x)
             historial.append(x)
             operacion = peticion_de_operacion(total_)
-            print(pos_his)
-            menu_operaciones(operacion , pos_his)
+            menu_operaciones(operacion , pos_his, total_)
         else:
             print("Invalido")
 
-def visualizar_historial(historial):
+def visualizar_historial(historial : list):
+    """
+    Devuelve en pantalla el historial de operaciones de la sesion
+    
+    Args:
+        historial : list    Historial de la sesion actual
+    """
     if len(historial) != 0:
         print(len(historial))
         for i in historial:
             print(i)
-            print()
     else:
         print("No hay datos en el historial ", len(historial))
 
@@ -149,6 +212,10 @@ def visualizar_historial(historial):
     menu_principal()
 
 def menu_principal():
+    """
+    Menu principal donde el usuario elige su accion
+
+    """
     global historial
     opciones = ["1", "2", "3"]
     print(" "*10 + " Calculadora " + " "*10)
@@ -166,9 +233,17 @@ def menu_principal():
             exit()
 
 def main():
+    """
+    Ejecucion post-validacion
+
+    """
     menu_principal()
     
 
 if __name__ == "__main__":
+    """
+    Validacion para ejecucion
+
+    """
     main()
     
